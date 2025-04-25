13f23db445eb1e372ce1526b1bf9ee905ef19f40
ALUNO C30 <alunoc30@unifor.local>
2025-04-24 21:54:51 -0300

    adição de moment

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 console.log(`Servidor rodando na porta ${PORT}`);
 });
+app.get('/info', (req, res) => {
+    res.json({ linguagem: 'Node.js', biblioteca: 'Express' });
+    });
+    const moment = require('moment');
+    console.log("Data atual:", moment().format('DD/MM/YYYY HH:mm:ss'));
